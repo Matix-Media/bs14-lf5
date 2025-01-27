c79fedba242ced464a6a0d044713bcf4e05e89c3
Matix-Media <heilmann@matix-media.net>
2025-01-27 11:42:59 +0100

    fix: snytax error - added missing brackets

--- server/db/scripts/003_alter_table_columns_auto_increment.sql
+++ server/db/scripts/003_alter_table_columns_auto_increment.sql
@@ -1,51 +1,26 @@
 ALTER TABLE kunde
-ALTER kundennr ADD GENERATED ALWAYS AS IDENTITY;
+    ALTER kundennr ADD GENERATED ALWAYS AS IDENTITY;
 
 ALTER TABLE lieferant
-ALTER lieferantennr ADD GENERATED ALWAYS AS IDENTITY;
+    ALTER lieferantennr ADD GENERATED ALWAYS AS IDENTITY;
 
 ALTER TABLE zutat
-ALTER zutatennr ADD GENERATED ALWAYS AS IDENTITY;
+    ALTER zutatennr ADD GENERATED ALWAYS AS IDENTITY;
 
-SELECT
-    SETVAL (
-        SELECT
-            pg_get_serial_sequence ('kunde', 'kundennr'),
-            (
-                SELECT
-                    max(kundennr)
-                FROM
-                    kunde
-            ),
-            true
-    );
+SELECT SETVAL(
+               (SELECT pg_get_serial_sequence('kunde', 'kundennr')),
+               (SELECT max(kundennr) FROM kunde),
+               true
+       );
 
-SELECT
-    SETVAL (
-        (
-            SELECT
-                pg_get_serial_sequence ('lieferant', 'lieferantennr')
-        ),
-        (
-            SELECT
-                max(lieferantennr)
-            FROM
-                lieferant
-        ),
-        true
-    );
+SELECT SETVAL(
+               (SELECT pg_get_serial_sequence('lieferant', 'lieferantennr')),
+               (SELECT max(lieferantennr) FROM lieferant),
+               true
+       );
 
-SELECT
-    SETVAL (
-        (
-            SELECT
-                pg_get_serial_sequence ('zutat', 'zutatennr')
-        ),
-        (
-            SELECT
-                max(zutatennr)
-            FROM
-                zutat
-        ),
-        true
-    );
+SELECT SETVAL(
+               (SELECT pg_get_serial_sequence('zutat', 'zutatennr')),
+               (SELECT max(zutatennr) FROM zutat),
+               true
+       );

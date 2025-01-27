7b0eb951f922d6e6b9f90067d4d2da9eaf780ac9
Raphael <71286185+Raphael9886@users.noreply.github.com>
2025-01-27 13:52:51 +0100

    insert ernaehrungskategorien

---
+++ server/db/migrations/006_insert_into ernaehrungskategorie.sql
@@ -0,0 +1,6 @@
+INSERT INTO ERNAEHRUNGSKATEGORIE (NAME, BESCHREIBUNG) VALUES
+                                                          ('Vegan', 'Vegane Produkte'),
+                                                          ('Vegetarisch', 'Vegetarische Produkte'),
+                                                          ('Glutenfrei', 'Glutenfreie Produkte'),
+                                                          ('Low-Carb', 'Low-Carb'),
+                                                          ('Protein-Reich', 'Proteinreiche Produkte');

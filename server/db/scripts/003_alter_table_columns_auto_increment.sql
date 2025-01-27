db16aa183b27a3e57455054f5cd7e41348427723
Matix-Media <heilmann@matix-media.net>
2025-01-27 11:28:00 +0100

    fix: adjusted migration to update set sequence to the max value

--- server/db/scripts/003_alter_table_columns_auto_increment.sql
+++ server/db/scripts/003_alter_table_columns_auto_increment.sql
@@ -6,3 +6,46 @@ ALTER lieferantennr ADD GENERATED ALWAYS AS IDENTITY;
 
 ALTER TABLE zutat
 ALTER zutatennr ADD GENERATED ALWAYS AS IDENTITY;
+
+SELECT
+    SETVAL (
+        SELECT
+            pg_get_serial_sequence ('kunde', 'kundennr'),
+            (
+                SELECT
+                    max(kundennr)
+                FROM
+                    kunde
+            ),
+            true
+    );
+
+SELECT
+    SETVAL (
+        (
+            SELECT
+                pg_get_serial_sequence ('lieferant', 'lieferantennr')
+        ),
+        (
+            SELECT
+                max(lieferantennr)
+            FROM
+                lieferant
+        ),
+        true
+    );
+
+SELECT
+    SETVAL (
+        (
+            SELECT
+                pg_get_serial_sequence ('zutat', 'zutatennr')
+        ),
+        (
+            SELECT
+                max(zutatennr)
+            FROM
+                zutat
+        ),
+        true
+    );

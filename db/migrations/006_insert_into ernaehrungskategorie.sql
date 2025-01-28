ce823b9b6d51c9911929147c697f95fad27c43ac
raphaelgrahl <raphaelgrahl@gmail.com>
2025-01-28 09:56:12 +0100

    added comments to migration

--- db/migrations/006_insert_into ernaehrungskategorie.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO ERNAEHRUNGSKATEGORIE (NAME, BESCHREIBUNG) VALUES
-                                                          ('Vegan', 'Vegane Produkte'),
-                                                          ('Vegetarisch', 'Vegetarische Produkte'),
-                                                          ('Glutenfrei', 'Glutenfreie Produkte'),
-                                                          ('Low-Carb', 'Low-Carb'),
-                                                          ('Protein-Reich', 'Proteinreiche Produkte');
